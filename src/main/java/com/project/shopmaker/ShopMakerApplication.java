53d5da4ba8022952ec106a75302effa0fb69cf99
takahiroaraki <s01110002e1117@gmail.com>
2021-04-06 13:41:48 +0900

    hello world実装

--- src/main/java/com/project/shopmaker/ShopMakerApplication.java
+++ src/main/java/com/project/shopmaker/ShopMakerApplication.java
@@ -2,12 +2,20 @@ package com.project.shopmaker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ShopMakerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopMakerApplication.class, args);
+	}
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello world";
 	}
 
 }
