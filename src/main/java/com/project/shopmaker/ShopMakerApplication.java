6b21736094c19d0bba6545dad715b8e935ce77aa
takahiroaraki <s01110002e1117@gmail.com>
2021-04-06 13:20:00 +0900

    first commit

---
+++ src/main/java/com/project/shopmaker/ShopMakerApplication.java
@@ -0,0 +1,13 @@
+package com.project.shopmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopMakerApplication.class, args);
+	}
+
+}
